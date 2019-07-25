fbb27c47d329dc467de8461bd78adf0135d2c049
dedynggego <dedynggego@gmail.com>
2019-07-25 16:37:56 +0700

    Update Artificial Neural Network.py

--- Artificial Neural Network.py
+++ Artificial Neural Network.py
@@ -25,3 +25,18 @@ hidden_to_output_weight = 2*(random.random((hidden_node, output_node)) - 0.5)
 
 print(input_to_hidden_weight)
 print(hidden_to_output_weight)
+
+#training
+for ep in range(epoch):
+    #forward propagation
+    output_hidden = 1/(1 + exp (-(dot(input_to_hidden_weight, inputs))))
+    output_ann = 1/(1 + exp (-(dot(hidden_to_ouput_weight, output_hidden))))
+    
+    #backpropagation
+    error_output = output - ouput_ann
+    error_hidden = dot(hidden_to_output_weight.T, error_output)
+    hidden_to_output_weight += learning_rate*dot(error_output*output_ann*(1-output_ann))
+    input_to_hidden_weight += learning_rate*dot(error_hidden*output_hidden*(1-output_hidden))
+    
+#evaluation
+print("True Output: {}\nArtificial Neural Network Output: {}\Accuracy: {}%".format(output.T, output_ann.T, (1-(output[argmax(output)]-output_ann[argmax(output_ann)]))[0]*100.0))
